4428eddd7b99dd1b377d0a82f745ede59d9cabc4
svkalidas <svkalidas@gmail.com>
2025-06-12 14:08:33 +0200

    Adding required dependencies and test class

---
+++ src/main/java/com/digital/wordcounter/service/SimpleWordCountService.java
@@ -0,0 +1,11 @@
+package com.digital.wordcounter.service;
+
+public class SimpleWordCountService implements WordCountService{
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int countWords(String inputText) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/digital/wordcounter/service/SimpleWordCountServiceTest.java
@@ -0,0 +1,4 @@
+package com.digital.wordcounter.service;
+
+public class SimpleWordCountServiceTest {
+}
